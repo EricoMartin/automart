877936b5c64bae4aa869bcf7aa5f6c4ab3e38d01
Eric Ibu <martinirex@yahoo.co.uk>
2019-07-01 21:33:24 +0100

    flag tests passing

--- server/test/mock_db/flags.js
+++ server/test/mock_db/flags.js
@@ -1,11 +1,12 @@
-export const flag = [
+const flag = [
   
   {
     id: 1,
     car_id: 2, 
     created_on: '10/10/2018',
     reason: 'fraudulent',
-    description: 'fake advert'
+    description: 'fake advert',
+    status: 'pending'
   },
 
   {
@@ -13,6 +14,27 @@ export const flag = [
     car_id: 3,
     created_on: '11/12/2018',
     reason: 'fraudulent',
-    description: 'damaged vehicle'
+    description: 'damaged vehicle',
+    status: 'pending',
+  },
+
+  {
+    id: 3,
+    car_id: 8,
+    created_on: '11/12/2018',
+    reason: 'fraudulent',
+    description: 'damaged vehicle',
+    status: 'pending',
+  },
+
+  {
+    id: 4,
+    car_id: 9,
+    created_on: '11/12/2018',
+    reason: 'fraudulent',
+    description: 'fake advert',
+    status: 'pending',
   }
 ]
+
+export default flag;
