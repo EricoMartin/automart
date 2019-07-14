1d1e6e828c3e6d06af04271d69343cabfbc52fde
EricoMartin <47243793+EricoMartin@users.noreply.github.com>
2019-07-14 19:04:23 +0100

    Update car-spec.js

--- server/test/car-spec.js
+++ server/test/car-spec.js
@@ -1,4 +1,4 @@
-/*import chai from 'chai';
+import chai from 'chai';
 import Car from '../models/car';
 import carsData from './mock_db/cars';
 
@@ -121,4 +121,3 @@ describe('car Endpoint', () => {
     });
   });
 });
-*/
