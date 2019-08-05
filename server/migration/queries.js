c5c2d5268d71ff5135794d34f9e9166e535ca803
Eric Ibu <martinirex@yahoo.co.uk>
2019-08-05 22:44:55 +0100

    bug (DATABASE_ENVIRONMENT CONFIGURATION)

--- server/migration/queries.js
+++ server/migration/queries.js
@@ -3,27 +3,25 @@ import dotenv from 'dotenv';
 
 
 dotenv.config();
-
-if(process.env.NODE_ENV === 'production') {
-
-const pool = new Pool({
-  user: process.env.DATABASE_USER,
-  host: process.env.DATABASE_HOST,
-  port: process.env.DATABASE_PORT,
-  database: process.env.DATABASE_NAME,
-  password: process.env.DATABASE_PASSWORD,
-});
-
+/* eslint-disable */
+if (process.env.NODE_ENV === 'production') {
+  const pool = new Pool({
+    user: process.env.DATABASE_USER,
+    host: process.env.DATABASE_HOST,
+    port: process.env.DATABASE_PORT,
+    database: process.env.DATABASE_NAME,
+    password: process.env.DATABASE_PASSWORD,
+  });
 } else {
   const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-});
-  
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+  });
 }
+ 
 pool.connect();
 
 export default {
@@ -37,3 +35,4 @@ export default {
     });
   },
 };
+/* eslint-enable */
